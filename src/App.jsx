13c29893b33075b85922604878eb19bc34253bd2
mannuccimattia <mannuccimattia@gmail.com>
2025-05-12 14:44:44 +0200

    [UPD] Done genre select - static

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 
+const movies = [
+  {
+    id: 1,
+    title: 'Inception',
+    genre: 'Fantascienza'
+  },
+  {
+    id: 2,
+    title: 'Il Padrino',
+    genre: 'Thriller'
+  },
+  {
+    id: 3,
+    title: 'Titanic',
+    genre: 'Romantico'
+  },
+  {
+    id: 4,
+    title: 'Batman',
+    genre: 'Azione'
+  },
+  {
+    id: 5,
+    title: 'Interstellar',
+    genre: 'Fantascienza'
+  },
+  {
+    id: 6,
+    title: 'Pulp Fiction',
+    genre: 'Thriller'
+  },
+]
+
 const App = () => {
   return (
-    <div>
-      wewe
+    <div className="container">
+      <h1>Movie List</h1>
+      <div className="form-floating">
+        <select
+          className="form-select form-select-lg"
+          aria-label="Default select example"
+        >
+          <option selected id='genreSelect'>Tutti i generi</option>
+          <option value="1">Azione</option>
+          <option value="2">Fantascienza</option>
+          <option value="3">Thriller</option>
+          <option value="4">Romantico</option>
+        </select>
+        <label for="genreSelect">Genere:</label>
+      </div>
+
     </div>
   )
 }
